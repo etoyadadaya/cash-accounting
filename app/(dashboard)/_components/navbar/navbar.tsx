1c56a4a6711712e96da68733285285e69b3b6e02
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-09-25 18:49:09 +0700

    added sidebar and navbar

---
+++ app/(dashboard)/_components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import { MobileSidebar } from "@/app/(dashboard)/_components/navbar/mobile-sidebar";
+
+import {NavbarRoutes} from "@/components/core/navbar-routes";
+
+export const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+      <NavbarRoutes />
+    </div>
+  );
+};
