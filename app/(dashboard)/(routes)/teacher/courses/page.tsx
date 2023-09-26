64fedfdc0f55d0c6196ce3109948e3e7d8d479e4
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-09-26 13:43:54 +0700

    course creation ui

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 const CoursesPage = () => {
   return (
-    <div>
-      Courses Page
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>
+          New Course
+        </Button>
+      </Link>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
 import React from "react";
+import {ToastProvider} from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
