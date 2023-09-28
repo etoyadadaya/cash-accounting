781fd116c51c84403a406ad1b678a6e5440c4371
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-09-28 17:50:13 +0700

    chapter access form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,7 +2,7 @@ import {auth} from "@clerk/nextjs";
 import {redirect} from "next/navigation";
 import {db} from "@/lib/db";
 import Link from "next/link";
-import {ArrowLeft, LayoutDashboard} from "lucide-react";
+import {ArrowLeft, Eye, LayoutDashboard, Video} from "lucide-react";
 import {IconBadge} from "@/components/core/icon-badge";
 import {
   ChapterTitleForm
@@ -10,6 +10,9 @@ import {
 import {
   ChapterDescriptionForm
 } from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form";
+import {
+  ChapterAccessForm
+} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form";
 
 const ChapterIdPage = async ({ params }: { params: { courseId: string, chapterId: string } }) => {
   const { userId } = auth();
@@ -86,6 +89,27 @@ const ChapterIdPage = async ({ params }: { params: { courseId: string, chapterId
               courseId={params.courseId}
               chapterId={params.chapterId}
             />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">
+                Access Settings
+              </h2>
+            </div>
+            <ChapterAccessForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={Video} />
+            <h2 className="text-xl">
+              Add a video
+            </h2>
           </div>
         </div>
       </div>
