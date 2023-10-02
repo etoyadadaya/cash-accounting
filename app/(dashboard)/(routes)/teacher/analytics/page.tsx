88220f3d2e0b7b17a4cd8dedb3cd816e76bae8bb
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-02 13:35:04 +0700

    teacher analytics dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import {auth} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+import {getAnalytics} from "@/actions/get-analytics";
+import {DataCard} from "@/app/(dashboard)/(routes)/teacher/analytics/_components/data-card";
+import {Chart} from "@/app/(dashboard)/(routes)/teacher/analytics/_components/chart";
 
-const AnalyticsPage = () => {
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalSales, totalRevenue } = await getAnalytics(userId);
+
   return (
-    <div>
-      Analytics page
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Revenue"
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard
+          label="Total Sales"
+          value={totalSales}
+        />
+      </div>
+      <Chart
+        data={data}
+      />
     </div>
   );
 };
