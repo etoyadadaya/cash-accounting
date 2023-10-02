7be77c0c5ae768b86a9c95bc920b1bde54a4fe36
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-02 13:03:26 +0700

    student dashboard

---
+++ app/(dashboard)/(routes)/(root)/_components/info-card.tsx
@@ -0,0 +1,28 @@
+import {IconBadge} from "@/components/core/icon-badge";
+import {LucideIcon} from "lucide-react";
+
+interface InfoCardProps {
+  icon: LucideIcon;
+  numberOfItems: number;
+  variant?: "default" | "success";
+  label: string;
+}
+
+export const InfoCard = ({ icon: Icon, variant, label, numberOfItems }: InfoCardProps) => {
+  return (
+    <div className="border rounded-md flex items-center gap-x-2 p-3">
+      <IconBadge
+        variant={variant}
+        icon={Icon}
+      />
+      <div>
+        <p className="font-medium">
+          {label}
+        </p>
+        <p className="text-gray-500 text-sm">
+          {numberOfItems} {numberOfItems === 1 ? "Course" : "Courses"}
+        </p>
+      </div>
+    </div>
+  );
+};
